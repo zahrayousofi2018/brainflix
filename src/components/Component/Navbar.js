19bca0f0f33e7d03a56bde76540cdc44f19a1563
Zahra Yousofi <zahra.yousofi.2010@gmail.com>
2022-02-02 21:45:13 -0500

    commit

--- src/components/Component/Navbar.js
+++ src/components/Component/Navbar.js
@@ -9,11 +9,11 @@ function Navbar() {
         </div>
 
         <div className="BrainFlix__second__container">  
+            <img src="Images/Mohan-muruge.jpg" className="Mohan__nav__placeholder__two" alt="Mohan placeholder"></img>
            
-           <img className="search__icon" src="Icons/search.svg" alt="search__icon"></img>
-    
            <input className="search__box" type="text" placeholder= "Search..." ></input>
-           {/* <img src="Images/Mohan-muruge.jpg" className="Mohan__nav__placeholder__two" alt="Mohan placeholder"></img> */}
+          <img className="search__icon" src="Icons/search.svg" alt="search__icon"></img>
+    
          
            
             <button  type="button" className="navbar__button">
