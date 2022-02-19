d24dfc386bfe810b4622bfde9e05137936237e0e
Zahra Yousofi <zahra.yousofi.2010@gmail.com>
2022-02-19 13:30:26 -0500

    text

--- src/components/Component/Uploads.js
+++ src/components/Component/Uploads.js
@@ -5,37 +5,39 @@ import "./Uploads.scss";
 function Uploads(props) {
 
    return(
-       <div>
-        
-
-       <div>
-           <hr></hr>
-           <h1 className="heading__text">Upload Video</h1>  
-       </div>
+    <div>
+           <div>
+             <hr></hr>
+             <h1 className="heading__text">Upload Video</h1>  
+           </div>
       
-       <div class="Uploads__video">
-           <label className="uploads__label">VIDEO THUMBNAIL</label>
-           <img src="Images/Upload-video-preview.jpg" alt="Uploads__Image"className="Uploads__image"/>
-       </div>
+        <div className="uploads__desktop__container">
+        
+             <div class="Uploads__video">
+              <label className="uploads__label">VIDEO THUMBNAIL</label>
+              <img src="Images/Upload-video-preview.jpg" alt="Uploads__Image"className="Uploads__image"/>
+            </div>
 
-      <div>
-       <label className="uploads__label__title">TITLE YOUR VIDEO</label>
-          <input className="new__comment__box" type="text" placeholder= "Add a title to your video..."
-        ></input>
-        </div>
-        <div>
+          <div>
+           <div>
+             <label className="uploads__label__title">TITLE YOUR VIDEO</label>
+             <input className="uploads__comment__box" type="text" placeholder= "Add a title to your video..." ></input> 
+           </div>
 
-        <label className="uploads__label__description">ADD A VIDEO DESCRIPTION</label>
-          <input className="new__comment__box" type="text" placeholder= "Add a description to your video..."
-        ></input>
-        </div>
-        <hr></hr>
-        <div>
+           <div>
+             <label className="uploads__label__description">ADD A VIDEO DESCRIPTION</label>
+             <input className="uploads__comment__box__description" type="text" placeholder= "Add a description to your video..." ></input>
+            </div>
+         </div>
+         </div>
+        
+         <div className="">
             <button className="cancel__button__one"type="button">CANCEL</button>
             <button className="publish__button"type="button">PUBLISH</button>
             <button className="cancel__button__two"type="button">CANCEL</button>
-        </div>
-        </div>
+         </div>
+     </div>
+    
        
    );
 }
