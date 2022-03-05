fd37df0c8f83fd8dae6eb7f5164f1a570404dc58
Zahra Yousofi <zahra.yousofi.2010@gmail.com>
2022-03-05 18:30:52 -0500

    commit

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const videos = require('./routes/videos')
 
 require('dotenv').config();
 const PORT = process.env.PORT || 9000;
@@ -7,6 +9,20 @@ const PORT = process.env.PORT || 9000;
 app.get('/', (_req, res) => {
     res.send('Hello world');
 });
+
+
+
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    next();
+});
+
+app.use('/static', express.static('files'));
+
+app.use('notes', videos);
+
 
 
 
---
+++ server/routes/videos.js
@@ -0,0 +1,25 @@
+const express = require ("express");
+const router = express.Router();
+const fs = require("fs");
+
+
+function readVideos () {
+    const videosData = fs.readFileSync("./data/videos.json");
+    const parsedData = JSON.parse(videosData);
+    return parsedData;
+}
+
+router.get("/", (_, res)=> {
+    res.json(readVideos());
+});
+
+router.get("/:videoId", (req, res)=> {
+    const videos = readVideos();
+    const singleVideo = videos.find(video => video.id === req.params.nodeId);
+    res.json(singleVideo)
+})
+
+
+
+
+module.exports = router;
